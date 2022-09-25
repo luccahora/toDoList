7f22e5857a20cb5887378a330fbd649a4b6ff6f8
Lucca Hora <luccahora@gmail.com>
2022-09-24 23:22:55 -0300

    chore: themes settings

---
+++ toDoList/src/styles/theme.ts
@@ -0,0 +1,31 @@
+export const THEME = {
+  colors: {
+    product: {
+      purple: "#8284FA",
+      purpleDark: "#5E60CE",
+      blue: "#4EA8DE",
+      blueDark: "#1E6F9F",
+    },
+    base: {
+      gray: {
+        700: "#0D0D0D",
+        600: "#1A1A1A",
+        500: "#262626",
+        400: "#333333",
+        300: "#808080",
+        200: "#D9D9D9",
+        100: "#F2F2F2",
+      },
+      danger: "#E25858",
+    },
+  },
+  fonts: {
+    title: "Inter_700Bold",
+    description: "Inter_400Regular",
+  },
+  fontSizes: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+  },
+};
