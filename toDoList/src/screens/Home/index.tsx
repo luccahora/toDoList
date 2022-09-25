32395346c0a0b1170e721a98852d2cc445b6f589
Lucca Hora <luccahora@gmail.com>
2022-09-25 01:18:37 -0300

    chore: input and counter

--- toDoList/src/screens/Home/index.tsx
+++ toDoList/src/screens/Home/index.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from "react";
+import { View, TextInput, TouchableOpacity, Image, Text } from "react-native";
+import { Header } from "../../components/Header";
+import { THEME } from "../../styles/theme";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
 export function Home() {
-  return (
-    <View>
+  const Counter = () => {
+    return (
+      <View style={styles.counter}>
+        <Text style={styles.counterText}>0</Text>
+      </View>
+    );
+  };
 
-    </View>
+  return (
+    <>
+      <Header />
+      <View style={styles.container}>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            placeholder="Adicione uma nova tarefa"
+            placeholderTextColor={THEME.colors.base.gray[300]}
+            selectionColor={THEME.colors.product.purpleDark}
+          />
+          <TouchableOpacity style={styles.button}>
+            <Image source={require("../../assets/ButtonPlus.png")} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.tasksInfo}>
+          <View style={styles.info}>
+            <Text style={styles.infoCreatedText}>Criadas</Text>
+            <Counter />
+          </View>
+          <View style={styles.info}>
+            <Text style={styles.infoDone}>Concluídas</Text>
+            <Counter />
+          </View>
+        </View>
+      </View>
+    </>
   );
 }
--- toDoList/src/screens/Home/styles.ts
+++ toDoList/src/screens/Home/styles.ts
@@ -1,3 +1,70 @@
-import {StyleSheet} from "react-native"
+import { StyleSheet } from "react-native";
+import { THEME } from "../../styles/theme";
 
-export const styles = StyleSheet.create({})
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: THEME.colors.base.gray[600],
+    paddingHorizontal: 20,
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: "-7%",
+    marginBottom: 35,
+  },
+  input: {
+    flex: 1,
+    height: 54,
+    backgroundColor: THEME.colors.base.gray[500],
+    borderRadius: 5,
+    color: THEME.colors.base.gray[100],
+    padding: 16,
+    fontSize: 16,
+    marginRight: 12,
+    borderWidth: 1,
+    borderColor: THEME.colors.base.gray[700],
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 52,
+    backgroundColor: THEME.colors.product.blueDark,
+    borderRadius: 6,
+  },
+  tasksInfo: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomColor: THEME.colors.base.gray[400],
+    borderBottomWidth: 1,
+  },
+  info: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  counter: {
+    width: 25,
+    height: 19,
+    backgroundColor: THEME.colors.base.gray[400],
+    borderRadius: 999,
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 10,
+  },
+  counterText: {
+    fontFamily: THEME.fonts.title,
+    fontSize: THEME.fontSizes.xs,
+    color: THEME.colors.base.gray[100],
+  },
+  infoCreatedText: {
+    fontFamily: THEME.fonts.title,
+    fontSize: THEME.fontSizes.md,
+    color: THEME.colors.product.blueDark,
+    marginBottom: 22,
+  },
+  infoDone: {
+    fontFamily: THEME.fonts.title,
+    fontSize: THEME.fontSizes.md,
+    color: THEME.colors.product.purpleDark,
+  },
+});
