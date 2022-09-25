e25fc61c3d578bd475af46d89b1f13e8091d40e4
Lucca Hora <luccahora@gmail.com>
2022-09-25 00:07:43 -0300

    chore: header

---
+++ toDoList/src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Image, SafeAreaView } from "react-native";
+
+import { styles } from "./styles";
+
+export function Header() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={require("../../assets/Logo.png")} />
+    </SafeAreaView>
+  );
+}
---
+++ toDoList/src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { THEME } from "../../styles/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: THEME.colors.base.gray[700],
+    height: "20%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
